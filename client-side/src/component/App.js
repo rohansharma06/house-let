a4843fc6a95f6664977eadb43ff700461f5db5ca
rohansharma06 <rohansharma251998@gmail.com>
2020-11-08 11:53:47 +0530

    update UI

--- client-side/src/component/App.js
+++ client-side/src/component/App.js
@@ -1,14 +1,36 @@
-
 import React, { Component } from 'react';
-import { Header,Landing } from './';
+import Header from './header';
+import Landing from './landing';
+import { Login ,Signup,home } from './seller';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="bg-black">
-        <Header />
-        <Landing />
-      </div>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path='/' render={()=>{
+              return (
+                <div>
+                  <Header />
+                  <Landing />
+                </div>
+              );
+            }} />
+            <Route exact path='/admin/login' component={Login} />
+            <Route exact path='/admin/signup' component={Signup} />
+            <Route exact path='/admin/home' component={home} />
+          </Switch>
+          
+        </div>
+      </Router>
+      
     );
   }
 }
