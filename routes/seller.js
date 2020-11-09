6b291ccd1c25a86fc781f81df9281155d5383ede
rohansharma06 <rohansharma251998@gmail.com>
2020-11-09 10:51:34 +0530

    router to display all property

--- routes/seller.js
+++ routes/seller.js
@@ -7,6 +7,6 @@ const property_contoller = require('../controllers/property_controller');
 router.post('/register', seller_controller.register);
 router.post('/login', seller_controller.login);
 router.post('/addproperty', passport.authenticate('jwt',{session: false}), property_contoller.create);
-
+router.get('/allproperty',passport.authenticate('jwt',{session: false}), property_contoller.fetchproperty);
 
 module.exports = router;
