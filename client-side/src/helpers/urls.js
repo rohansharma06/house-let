826bb5edffd60b61377f49989eb612d970dc66a6
rohansharma06 <rohansharma251998@gmail.com>
2020-11-09 10:53:27 +0530

    add url

--- client-side/src/helpers/urls.js
+++ client-side/src/helpers/urls.js
@@ -3,5 +3,6 @@ const API_ROOT = 'http://localhost:8000';
 export const APIUrls ={
     adminLogin: () => `${API_ROOT}/admin/login`,
     adminSignup: () => `${API_ROOT}/admin/register`,
-    addproperty:() => `${API_ROOT}/admin/addproperty`,
+    addproperty: () => `${API_ROOT}/admin/addproperty`,
+    fetchproperty: () => `${API_ROOT}/admin/allproperty`,
 }
