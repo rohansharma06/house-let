586b84fb15e2753c0618be23996ef9d41d1f0ad8
rohansharma06 <rohansharma251998@gmail.com>
2020-11-09 10:55:50 +0530

    fetch all property

--- client-side/src/component/seller/allProperties.js
+++ client-side/src/component/seller/allProperties.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
 
 class allProperties extends Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
+        // const {} = this.props.data;
+        console.log('alldata:',this.props.property);
+        const {name,city,rent,bhk,description,phone} = this.props.property;
         return (
-           
             <div className="card" style={{width: "18rem",margin:"5px 5px 5px 5px",display:"inline-block"}}>
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQyWmTKjbzc5iD-b9RrkzL7dtGQf_pOxghr4Q&usqp=CAU" className="card-img-top" alt="..." />
                 <div className="card-body">
-                    <h5 className="card-title">Aman Rai || <i>Delhi</i> </h5>
-                    <p className="card-text" style={{lineHeight:"2px"}}>
-                        <p>Size:1Bhk</p>
-                        <p>Rent:20,000 p/m</p>
-                        <p>Contact No: 7676767678</p>
+                    <h5 className="card-title">{name} || <i>{city}</i> </h5>
+                    {/* <p className="card-text" style={{lineHeight:"3px"}}> */}
+                    <p className="card-text">
+                        <span className="bg-blue">Size/BHK</span>:{bhk}, <span className="bg-blue">Rent</span>:{rent} p/m
                     </p>
-                    <p className="card-text">Fully furnish flat on the with beautiful area.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                    <p className="card-text"> <span className="bg-blue">Contact No</span>: {phone}</p>
+                    <p className="card-description" style={{fontSize:'13px'}}>{description}</p>
+                    
+                    
+                    <a href="#" className="btn btn-primary">Vew Applications</a>
                 </div>
             </div>
                 
