910bfe2a00a1633156b442328f4d3fc06ceb4e02
rohansharma06 <rohansharma251998@gmail.com>
2020-11-09 10:55:30 +0530

    add acton for fetch property

--- client-side/src/Action/property.js
+++ client-side/src/Action/property.js
@@ -2,6 +2,7 @@ import{
     ADD_PROPERTY_START,
     ADD_PROPERTY_SUCCESS,
     ADD_PROPERTY_FAILED,
+    FETCH_PROPERTY,
 } from './actionType';
 
 
@@ -49,5 +50,33 @@ export function addproperty(name,city,rent,phone,bhk,description){
             dispatch(addPropertyFailed('Server Error! Try again'));
         })
         
+    }
+}
+
+export function fetchPropertySuccess(data){
+    return {
+        type: FETCH_PROPERTY,
+        data
+    }
+}
+
+export function fetchProperty(){
+    return(dispatch) => {
+        const url = APIUrls.fetchproperty();
+        fetch(url,{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                Authorization: `Bearer ${getAuthTokenFromLocalStorage()}`, 
+            }
+        })
+        .then((response) => response.json())
+        .then(data => {
+            if(data.success){
+                console.log('fetchProperty:',data);
+                dispatch(fetchPropertySuccess(data.Property));
+            }
+            
+        })
     }
 }
