4b4f3b68a104523922f85abff598c7e6423ca801
rohansharma06 <rohansharma251998@gmail.com>
2020-11-10 09:08:15 +0530

    buyre profile

---
+++ client-side/src/component/buyer/profile.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {Header,DisplayApplyProperty} from './';
+import { data as propertyList } from './data';
+import { isUserToken } from '../../helpers/utils';
+import { Redirect } from 'react-router-dom';
+
+
+class profile extends Component {
+    render() {
+        // console.log('profile:',this.props.user);
+        if(!isUserToken()){
+            return <Redirect to='/user' />;
+        }
+        
+        return (
+            <div>
+                <Header />
+                {propertyList.length <=0 ? (<div className="loader"></div>) : (
+                    <div className="display-properties">
+                        {propertyList.map((data) => (
+                            <DisplayApplyProperty property={data} key={data._id} />
+                        ))}
+                    </div>
+                )}
+            </div>
+        );
+    }
+}
+
+function mapStoreToProps(state) {
+    return {
+      user: state.user,
+    };
+  }
+export default connect(mapStoreToProps)(profile);
